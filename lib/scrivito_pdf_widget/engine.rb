b0272cb2262d75271c5e632c2e35509896e1f2bb
Gert Geidel <gert.geidel@infopark.de>
2015-03-27 11:42:41 +0100

    Fix engine

--- lib/scrivito_pdf_widget/engine.rb
+++ lib/scrivito_pdf_widget/engine.rb
@@ -3,9 +3,9 @@ require 'font-awesome-rails'
 module ScrivitoPdfWidget
   class Engine < ::Rails::Engine
     isolate_namespace ScrivitoPdfWidget
-  end
 
-  initializer "ScrivitoPdfWidget.assets.precompile" do |app|
-    config.assets.precompile += %w(scrivito_pdf_widget/pdf.worker.js)
+    initializer "ScrivitoPdfWidget.assets.precompile" do |app|
+      config.assets.precompile += %w(scrivito_pdf_widget/pdf.worker.js)
+    end
   end
 end
