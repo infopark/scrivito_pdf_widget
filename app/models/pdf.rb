0ffe499aeb29b8019e720f7ce39833a5f16a7471
Gert Geidel <gert.geidel@infopark.de>
2015-05-21 17:39:17 +0200

    Remove created method. It is not necessary.

--- app/models/pdf.rb
+++ app/models/pdf.rb
@@ -1,24 +1,7 @@
-class Pdf < Scrivito::BasicObj
+class Pdf < Obj
   attribute :headline, :string
   attribute :description, :html
   attribute :blob, :binary
-
-  def self.create(attributes)
-    attributes = attributes.with_indifferent_access
-
-    unless attributes.has_key?(:_obj_class)
-      attributes[:_obj_class] = 'Pdf'
-    end
-
-    if attributes.has_key?(:blob) && !attributes.has_key?(:_path)
-      filename = Pathname.new(attributes[:blob].path).basename
-      path = "_resources/#{SecureRandom.hex(8)}/#{filename}"
-
-      attributes[:_path] = path
-    end
-
-    super(attributes)
-  end
 
   def url_to_pdf
     self.try(:binary_url).split('?').first
--- app/models/pdf_widget.rb
+++ app/models/pdf_widget.rb
@@ -2,6 +2,6 @@ class PdfWidget < Widget
   attribute :pdf, :reference
 
   def self.filter
-    {_obj_class: {field: '_obj_class', options: {images: {selected: true, title: 'Pdfs', value: 'Pdf'}}}}
+    {_obj_class: {field: '_obj_class', options: {images: {selected: true, title: 'PDFs', value: 'Pdf'}}}}
   end
 end
