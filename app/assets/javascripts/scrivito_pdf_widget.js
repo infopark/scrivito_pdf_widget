5fb29ea6f1dfb84a0efc400c486a74cd102f7d8b
Gert Geidel <gert.geidel@infopark.de>
2016-08-24 11:56:35 +0200

    better loading of js

--- app/assets/javascripts/scrivito_pdf_widget.js
+++ app/assets/javascripts/scrivito_pdf_widget.js
@@ -1,4 +1,2 @@
-//= require scrivito_pdf_widget/pdf
-//= require scrivito_pdf_widget/jquery.viewer
-//= require scrivito_pdf_widget/widget
+//= require scrivito_pdf_widget/application
 //= require scrivito_pdf_widget/editing
